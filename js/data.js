4930b5b62124930ef2f75c1ab404b1569256ac96
GonzaloMonteodorisio <56704895+GonzaloMonteodorisio@users.noreply.github.com>
2020-07-12 23:19:41 -0300

    guitar store inicial

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var data =  [
+
+					{
+						"id": "a" ,
+						"title": "Guitar Gracia C170-01",  
+						"image": "C170-01.jpg", 
+						"price" : 12000
+					},
+			
+					{
+						"id": "b",
+						"title": "Guitar Fender FA25-5E", 
+						"image": "fenderFa235E.jpg", 
+						"price": 15000 
+					},
+			
+					{
+						"id": "c",
+						"title": "Guitar Texas Cutaway", 
+						"image": "texasCutaway.jpg", 
+						"price": 20000 
+					},
+			
+					{
+						"id": "d",
+						"title": "Guitar Valencia VC102BUS", 
+						"image": "ValenciaVc102Bus.jpg", 
+						"price": 28000
+					},
+			
+					{
+						"id": "e",
+						"title": "Guitar Valencia VC104BK", 
+						"image": "valenciaVc104Bk.jpg", 
+						"price": 12000
+					}
+				];
+		
